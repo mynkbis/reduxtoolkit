79d2fc66197e4535eca631ed30beaabe543bbdf9
mynkbis <bisht.surya@ymail.com>
2022-06-29 10:11:34 +0530

    toolkit counter

---
+++ src/features/counter/counter.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { increment, decrement, reset, incrementByAmount } from './counterSlice'
+
+const Counter = () => {
+    const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
+  
+  const [incrementAmount, setIncrementAmount] = useState(0);
+  const addValue = Number(incrementAmount) || 0;
+  
+  const resetAll = () =>
+  {
+    setIncrementAmount(0);
+    dispatch(reset())
+    }
+  return (
+      <div>
+          <p>{count}</p>
+          <div>
+              <button onClick={()=>dispatch(increment())}>+</button>
+              <button onClick={() => dispatch(decrement())}>-</button>
+              <button onClick={()=>dispatch(reset)}>r</button>
+      </div>
+      <input type='text' value={incrementAmount} onChange={(e) => setIncrementAmount(e.target.value)} />
+      <div>
+        <button onClick={()=>dispatch(incrementByAmount(addValue))}>Add value</button>
+        <button onClick={resetAll}>Reset</button>
+      </div>
+    </div>
+  )
+}
+
+export default Counter
